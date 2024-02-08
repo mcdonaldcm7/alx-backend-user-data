8e0a8d7af9010510d7a9806f3cdd36345b2e7614
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-08 17:01:06 +0100

    Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Task
+
+5. Encrypting passwords
+
+User passwords should NEVER be stored in plain text in a database.
+
+Implement a hash_password function that expects one string argument name
+password and returns a salted, hashed password, which is a byte string.
+
+Use the bcrypt package to perform the hashing (with hashpw).
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Uses the bcrypt module to return a hashed version of password
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())

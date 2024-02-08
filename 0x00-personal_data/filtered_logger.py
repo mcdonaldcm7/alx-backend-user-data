0e1504e3ad918f0a347a8f14c11787422203c746
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-08 15:08:24 +0100

    Updated Documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,26 +2,24 @@
 """
 Task
 
-3. Connect to secure database
+4. Read and filter data
 
-Database credentials should NEVER be stored in code or checked into version
-control. One secure option is to store them as environment variable on the
-application server.
+Implement a main function that takes no arguments and returns nothing.
 
-In this task, you will connect to a secure holberton database to read a users
-table. The database is protected by a username and password that are set as
-environment variables on the server named PERSONAL_DATA_DB_USERNAME (set the
-default as “root”), PERSONAL_DATA_DB_PASSWORD (set the default as an empty
-string) and PERSONAL_DATA_DB_HOST (set the default as “localhost”).
+The function will obtain a database connection using get_db and retrieve all
+rows in the users table and display each row under a filtered format like this:
 
-The database name is stored in PERSONAL_DATA_DB_NAME.
+[HOLBERTON] user_data INFO 2019-11-19 18:37:59,596: name=***; email=***;
+phone=***; ssn=***; password=***; ip=e848:e856:4e0b:a056:54ad:1e98:8110:ce1b;
+last_login=2019-11-14T06:16:24; user_agent=Mozilla/5.0 (compatible; MSIE 9.0;
+Windows NT 6.1; WOW64; Trident/5.0; KTXN);
+Filtered fields:
 
-Implement a get_db function that returns a connector to the database
-(mysql.connector.connection.MySQLConnection object).
-
-Use the os module to obtain credentials from the environment
-Use the module mysql-connector-python to connect to the MySQL database
-(pip3 install mysql-connector-python)
+    - name
+    - email
+    - phone
+    - ssn
+    - password
 """
 from typing import List
 import re

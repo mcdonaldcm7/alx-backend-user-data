3a25495c3f6ead41f38358f9dc2bd5037f132cfb
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-08 12:42:48 +0100

    Added definition for get_logger method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,20 +2,27 @@
 """
 Task
 
-1. Log formatter
+2. Create logger
 
-Update the RedactingFormatter class to accept a list of strings fields
-constructor argument.
+Implement a get_logger function that takes no arguments and returns a
+logging.Logger object.
 
-Implement the format method to filter values in incoming log records using
-filter_datum. Values for fields in fields should be filtered.
+The logger should be named "user_data" and only log up to logging.INFO level.
+It should not propagate messages to other loggers. It should have a
+StreamHandler with RedactingFormatter as formatter.
 
-DO NOT extrapolate FORMAT manually. The format method should be less than 5
-lines long.
+Create a tuple PII_FIELDS constant at the root of the module containing the
+fields from user_data.csv that are considered PII. PII_FIELDS can contain only
+5 fields - choose the right list of fields that can are considered as
+“important” PIIs or information that you must hide in your logs. Use it to
+parameterize the formatter.
 """
 from typing import List
 import re
 import logging
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -25,6 +32,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         repl = f + '=' + redaction + separator
         message = re.sub((f + '=.*?' + separator), repl, message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and returns a logger after setting the properties according to the
+    specification
+    """
+    user_data = logging.getLogger("user_data")
+    user_data.propagate = False
+    user_data.setLevel(logging.INFO)
+
+    formatter = RedactingFormatter(PII_FIELDS)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    user_data.addHandler(handler)
+    return user_data
 
 
 class RedactingFormatter(logging.Formatter):

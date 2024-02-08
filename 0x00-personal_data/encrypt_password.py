e5dc1643341343a869cc70be9a81f776bcea9ea9
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-08 17:07:44 +0100

    Validate that the provided password matches the hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -19,3 +19,10 @@ def hash_password(password: str) -> bytes:
     Uses the bcrypt module to return a hashed version of password
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Uses bcrypt to validate that the provided password matches the hashed
+    password
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)

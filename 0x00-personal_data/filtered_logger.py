b09d09cda3c83ed8865b5173dc85605799631f15
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-08 12:43:58 +0100

    Changed PII_FIELDS constant to include password

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,7 +22,7 @@ import re
 import logging
 
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
+PII_FIELDS = ("email", "phone", "ssn", "ip", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,

9df538a977387d857fd9f8425c4fb796de811113
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-12 17:20:48 +0100

    Added definition for the 'require_auth' method

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -13,8 +13,15 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Unimplemented method for template
         """
+        Checks whether or not @path is in @excluded_paths and thus requires
+        authentication
+        """
+        if path is not None and path[len(path) - 1] != '/':
+            path += '/'
+        if (path is None or excluded_paths is None or len(excluded_paths) == 0
+                or path not in excluded_paths):
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:

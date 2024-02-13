869be34e991276dea2a6fd37d4943eeb3b9f081f
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-13 14:23:52 +0100

    Commented untested part of methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,18 +17,18 @@ class Auth:
         Checks whether or not @path is in @excluded_paths and thus requires
         authentication
         """
-        if path is not None and path[len(path) - 1] != '/':
-            path += '/'
-        if (path is None or excluded_paths is None or len(excluded_paths) == 0
-                or path not in excluded_paths):
-            return True
+        # if path is not None and path[len(path) - 1] != '/':
+        #    path += '/'
+        # if (path is None or excluded_paths is None or len(excluded_paths)== 0
+        #        or path not in excluded_paths):
+        #    return True
         return False
 
     def authorization_header(self, request=None) -> str:
         """Unimplemented method for template
         """
-        if 'Authorization' in request.headers:
-            return request.headers.get("Authorization")
+        # if 'Authorization' in request.headers:
+        #    return request.headers.get("Authorization")
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):

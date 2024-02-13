2aa1abe5178bfaf0049c7c4001657a5a9b95bd55
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-13 13:05:48 +0100

    Template for implementing Authentication classes

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -27,6 +27,8 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """Unimplemented method for template
         """
+        if 'Authorization' in request.headers:
+            return request.headers.get("Authorization")
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):

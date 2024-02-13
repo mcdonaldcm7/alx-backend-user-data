e67643dee10cc7b5d5131d4d3f97688367ccc44e
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-13 14:09:05 +0100

    Set strict_slashes to False

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
     """
     Endpoint for Route: GET /api/v1/unauthorized
@@ -34,7 +34,7 @@ def unauthorized():
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'])
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden():
     """
     Endpoint for Route: GET /api/v1/forbidden

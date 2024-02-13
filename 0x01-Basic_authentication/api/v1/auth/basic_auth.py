c1df555b93214339bb7d2daf61f0791a35c9f7b7
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-13 15:06:28 +0100

    Basic - Allow password with ':'

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -55,8 +55,8 @@ class BasicAuth(Auth):
                 decoded_base64_authorization_header):
             return (None, None)
 
-        credentials = decoded_base64_authorization_header.split(":")
-
+        credentials = decoded_base64_authorization_header.split(
+                ":", maxsplit=1)
         return (credentials[0], credentials[1])
 
     def user_object_from_credentials(self, user_email: str, user_pwd: str

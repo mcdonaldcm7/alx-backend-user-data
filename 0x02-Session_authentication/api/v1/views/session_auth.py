e625096898d10d0a9095cd75996994f916c585a5
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-15 14:59:48 +0100

    Added route for DELETE

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -40,3 +40,15 @@ def login() -> str:
     ret.set_cookie(cookie_name, session_id)
 
     return ret
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout() -> str:
+    """DELETE /api/v1/auth_session/logout
+    """
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200

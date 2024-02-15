9a6a340fd3d0a541605d161ba75da41b9f33df10
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-15 13:32:04 +0100

    If auth.authorization_header(request) and auth.session_cookie(request) return None, abort(401)

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,7 +37,8 @@ def before_request():
     if auth is None or not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
 
     current_user = auth.current_user(request)
@@ -47,9 +48,9 @@ def before_request():
     else:
         request.current_user = current_user
 
-    if (auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None):
-        abort(401)
+    # if (auth.authorization_header(request) is None and
+    #        auth.session_cookie(request) is None):
+    #    abort(401)
 
 
 @app.errorhandler(401)

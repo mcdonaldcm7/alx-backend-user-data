1148d80e83b83313c03d4fdf57f8602c2348247c
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-16 05:57:45 +0100

    UserSession class module

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for UserSession
+"""
+from .base import Base
+
+
+class UserSession(Base):
+    """UserSession Class
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a User instance
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")

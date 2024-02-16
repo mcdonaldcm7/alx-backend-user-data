b5733a132b86d0683640f5c895f8e59c05debbf5
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-16 11:19:48 +0100

    Corrected the user_id_for_session_id method to access variables from the stored file

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -11,6 +11,9 @@ class SessionDBAuth(SessionExpAuth):
     Child of SessionExpAuth class that uses a database to store info
     """
 
+    def __init__(self):
+        super().__init__()
+
     def create_session(self, user_id=None):
         """ Creates and Stores a new instance of UserSession and Returns the
         Session ID
@@ -36,23 +39,25 @@ class SessionDBAuth(SessionExpAuth):
         from models.user_session import UserSession
 
         user_id = None
+        created_at = None
+        if "UserSession" not in DATA:
+            return None
         session_db_objs = DATA["UserSession"]
         for k, v in session_db_objs.items():
             if v.session_id == session_id:
-                user_id = k
-
-        session_dictionary = self.user_id_by_session_id[session_id]
+                user_id = v.user_id
+                created_at = v.created_at
 
         from datetime import datetime, timedelta
 
         if self.session_duration <= 0:
-            return session_dictionary["user_id"]
-        if "created_at" not in session_dictionary:
+            return user_id
+        if created_at is None:
             return None
-        if ((session_dictionary["created_at"] + timedelta(
-                seconds=self.session_duration)) < datetime.now()):
+        if ((created_at + timedelta(seconds=self.session_duration)) <
+                datetime.utcnow()):
             return None
-        return session_dictionary["user_id"]
+        return user_id
 
     def destroy_session(self, request=None):
         """ Destroys the UserSession based on the Session ID from the request
@@ -64,9 +69,6 @@ class SessionDBAuth(SessionExpAuth):
         session_id = self.session_cookie(request)
         if session_id is None:
             return False
-        # if self.user_id_for_session_id(session_id) is None:
-        #    return False
-        # del self.user_id_by_session_id[session_id]
 
         from models.base import DATA
 

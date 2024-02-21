3aced850e48381394d00b3a2ecd9d48649c6a562
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-21 14:13:04 +0100

    update_password catches and raises ValueError exception

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -121,6 +121,8 @@ class Auth:
             hashed_password = _hash_password(password)
             self._db.update_user(user.id, hashed_password=hashed_password,
                                  reset_token=None)
-            return None
         except NoResultFound:
             raise ValueError()
+        except ValueError:
+            raise ValueError()
+        return None
